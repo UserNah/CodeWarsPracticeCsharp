ea1559b15f5a24aeed6d65879cfef51f1a372672
bauyrzhanAl <bauyrzhanaldakulov@gmail.com>
2020-09-18 22:05:20 +0600

    8 kyu - The Falling Speed Of Petals

--- CodeWarsPracticeCsharp/Program.cs
+++ CodeWarsPracticeCsharp/Program.cs
@@ -9,13 +9,7 @@ namespace CodeWarsPracticeCsharp
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("out : " + BlockSequence.solve(1));
-            //Console.WriteLine("out : " + BlockSequence.solve(2));
-            //Console.WriteLine("out : " + BlockSequence.solve(3));
-            //Console.WriteLine("out : " + BlockSequence.solve(100));
-            Console.WriteLine("out : " + BlockSequence.solve(2100));
-            //Console.WriteLine("out : " + BlockSequence.solve(999999999999999993));
-
+            Console.WriteLine(TheFallingSpeedOfPetals.SakuraFall(5));
         }
     }
 }
